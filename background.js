0a3ff05c7efe0d3c8ed14442b6ad46794cd6a76c
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-09-09 13:40:10 +0700

    Less permissions

--- background.js
+++ background.js
@@ -4,29 +4,35 @@ var cookies = chrome.cookies;
 
 var cookieName = "XDEBUG_SESSION";
 var cookieValue = "PHPSTORM";
-var enableDebugText = "Enable Debugging";
-var disableDebugText = "Disable Debugging";
+
+var setBadage = function (tabId, text, color, title) {
+    browserAction.setBadgeText({
+        text: text,
+        tabId: tabId
+    });
+
+    browserAction.setBadgeBackgroundColor({
+        color: color,
+        tabId: tabId
+    });
+
+    browserAction.setTitle({
+        title: title,
+        tabId: tabId
+    });
+};
 
 var enableXdebug = function (tab) {
     cookies.set({
         url: tab.url,
         name: cookieName,
         value: cookieValue
-    });
-
-    browserAction.setBadgeText({
-        text: "On",
-        tabId: tab.id
-    });
-
-    browserAction.setBadgeBackgroundColor({
-        color: '#2ecc71',
-        tabId: tab.id
-    });
-
-    browserAction.setTitle({
-        title: disableDebugText,
-        tabId: tab.id
+    }, function (cookie) {
+        if (cookie !== undefined) {
+            setBadage(tab.id, 'On', '#2ecc71', 'Enable Debugging');
+        } else {
+            setBadage(tab.id, 'Err', '#95a5a6', 'Error');
+        }
     });
 };
 
@@ -34,26 +40,17 @@ var disableXdebug = function (tab) {
     cookies.remove({
         url: tab.url,
         name: cookieName
-    });
-
-    browserAction.setBadgeText({
-        text: "Off",
-        tabId: tab.id
-    });
-
-    browserAction.setBadgeBackgroundColor({
-        color: '#e67e22',
-        tabId: tab.id
-    });
-
-    browserAction.setTitle({
-        title: enableDebugText,
-        tabId: tab.id
+    }, function (detail) {
+        if (detail !== undefined) {
+            setBadage(tab.id, 'Off', '#e67e22', 'Disable Debugging');
+        } else {
+            setBadage(tab.id, 'Err', '#95a5a6', 'Error');
+        }
     });
 };
 
 var gotCookie = function (cookie) {
-    if (cookie === null || cookie.value !== cookieValue) {
+    if (cookie === undefined || cookie.value !== cookieValue) {
         enableXdebug(this);
     } else {
         disableXdebug(this);
